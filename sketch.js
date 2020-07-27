f21c90c03fd26fb131c953d7836b1cc5c9548881
Abhilash C N <65819936+Abhilash2004@users.noreply.github.com>
2020-07-27 20:44:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var form,player,game 
+var database;
+ 
+function setup(){
+    createCanvas(500,500);
+     
+    database=firebase.database();
+    game=new Game()
+    game.getState()
+    game.start()
+     
+}
+
+function draw(){
+    background("white");
+    
+
+}
